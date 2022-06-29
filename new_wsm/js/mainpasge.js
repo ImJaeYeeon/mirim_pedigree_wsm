cbbd5d8200dd18a0bea9801fc45d29de0bf57aa6
ImJaeYeeon <w2109@e-mirim.hs.kr>
2022-06-29 15:57:39 +0900

    최종

--- new_wsm/js/mainpasge.js
+++ new_wsm/js/mainpasge.js
@@ -1,18 +1,17 @@
-const toggleMenu = (toggleID, navListID) => {
-    //html->js
-    const toggle = document.getElementById(toggleID);
-    const navlist = document.getElementById(navListID);
+const toggleMenu = (toggleId, navListId) => {
+    //html -> js
+    const toggle = document.getElementById(toggleId);
+    const navList = document.getElementById(navListId);
     const toggleIcon = toggle.getElementsByTagName("i")[0];
 
-    if (toggle && navlist) {
-        //toggle click
+    if (toggle && navList) {
         toggle.addEventListener('click', () => {
-            //show/hide menu: .show-menu
-            navlist.classList.toggle('show-menu');
-            //change toggle icon : bx-menu <-> bx-x
+            navList.classList.toggle('show-menu');
+
             toggleIcon.classList.toggle("bx-menu");
-            toggleIcon.classList.toggle("bxs-square");
+            toggleIcon.classList.toggle("bx-x");
         });
     }
+
 }
 toggleMenu("nav-toggle", "nav-list");
